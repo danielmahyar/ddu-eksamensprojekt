450fec4da440dbbc781458d339f40c0826bc94ba
danielmahyar <thediamonds764@gmail.com>
2022-04-02 01:03:05 +0200

    feat: :art: updated user creation

--- functions/src/auth/index.ts
+++ functions/src/auth/index.ts
@@ -1,22 +1,19 @@
-import { firestore } from "firebase-admin";
+import  { firestore, auth } from "firebase-admin";
 import * as functions from "firebase-functions";
 import { sendWelcomeEmail } from "../mail";
 
-
-export const welcomeEmail = functions.auth.user().onCreate(async (user, context) => {
-	//send email
+functions.firestore.document("users/{uid}").onCreate(async (snapshot, context) => {
+	const userData = snapshot.data()
 	try {
-		await sendWelcomeEmail(user.email || "", user.displayName || "Nye Bruger")
-		const data = {
-			photoURL: user.photoURL || "https://png.pngitem.com/pimgs/s/551-5510463_default-user-image-png-transparent-png.png",
-			email: user.email,
-			username: user.displayName || "Nye Bruger"
-		}
-		await firestore().collection('users').doc(user.uid).create(data)
+		await sendWelcomeEmail(userData.email, userData.displayName)
+		await auth().updateUser(context.params.uid, {
+			displayName: userData.displayName,
+			email: userData.email,
+		})
 	} catch (error) {
 		console.log(error)
 	}
-});
+})
 
 export const cleanupUserDelete = functions.auth.user().onDelete(async (user) => {
 	try {
